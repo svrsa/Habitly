a88157f0fadf29810af0bd06f878bf684fb67cb4
Stefan <stefan.vrs99@gmail.com>
2026-05-29 20:38:22 +0200

    feat: add study task database model

---
+++ app/src/main/java/com/example/habitly/data/local/DatabaseConstants.kt
@@ -0,0 +1,3 @@
+package com.example.habitly.data.local
+
+const val HABITLY_DATABASE_NAME = "habitly_database"
---
+++ app/src/main/java/com/example/habitly/data/local/HabitlyDatabase.kt
@@ -0,0 +1,12 @@
+package com.example.habitly.data.local
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.example.habitly.data.local.entity.StudyTaskEntity
+
+@Database(
+    entities = [StudyTaskEntity::class],
+    version = 1,
+    exportSchema = false
+)
+abstract class HabitlyDatabase : RoomDatabase()
