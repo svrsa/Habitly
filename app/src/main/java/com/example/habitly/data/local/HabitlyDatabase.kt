c1106b1bf4890d49ec7adebf5a574140e7d03e5f
Stefan <stefan.vrs99@gmail.com>
2026-06-14 21:44:49 +0200

    feat: add study session dao

--- app/src/main/java/com/example/habitly/data/local/HabitlyDatabase.kt
+++ app/src/main/java/com/example/habitly/data/local/HabitlyDatabase.kt
@@ -2,6 +2,7 @@ package com.example.habitly.data.local
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.example.habitly.data.local.dao.StudySessionDao
 import com.example.habitly.data.local.dao.StudyTaskDao
 import com.example.habitly.data.local.entity.StudySessionEntity
 import com.example.habitly.data.local.entity.StudyTaskEntity
@@ -16,4 +17,6 @@ import com.example.habitly.data.local.entity.StudyTaskEntity
 )
 abstract class HabitlyDatabase : RoomDatabase() {
     abstract fun studyTaskDao(): StudyTaskDao
+
+    abstract fun studySessionDao(): StudySessionDao
 }
